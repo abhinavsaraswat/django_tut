b459112726b727cc24513ea2271cba7e61229104
abhinavsaraswat <14240574+abhinavsaraswat@users.noreply.github.com>
2022-06-02 15:40:51 +0100

    created index and details pages

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
 
+from .models import Question
 # Create your views here.
 
 def index(request): 
-    return HttpResponse("hello, world!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id): 
+    try: 
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist: 
+        raise Http404("Question does not exist")
+    context = {
+        'question':question
+    }
+    return render(request, 'polls/index.html', context)
+
+def results(request, question_id): 
+    response = "you're looking at results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id): 
+    return HttpResponse("you're voting on question %s" % question_id)
